5d61c4f9a8c160728019e0e30dee82dd3e0fd628
Pachciar <pachciar3@gmail.com>
2021-02-10 16:19:03 +0100

    Improve FormMessages and add message to ListMessages

--- src/Messenger/components/ListMessages/index.js
+++ src/Messenger/components/ListMessages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Message from '../../components/Message';
+import FormMessage from '../../../components/FormMessage';
 
 function ListMessages({messages}) {
-  return messages.map(message => (
-    <Message key={`messenger-message-${message.id}`} message={message}></Message>
-  ));
+  if (messages.length) {
+    return messages.map(message => (
+      <Message key={`messenger-message-${message.id}`} message={message}></Message>
+    ));
+  }
+  return <FormMessage type="warning">Brak wiadomości</FormMessage>;
 }
 
 export default ListMessages;
